87a02d6d0e08be242815f32cf688d6f7f589e111
Ibrahim-hasanean <41992179+Ibrahim-hasanean@users.noreply.github.com>
2022-03-13 21:35:26 +0200

    modify add item schema

--- dest/Components/Admin/middleware/validateItem.js
+++ dest/Components/Admin/middleware/validateItem.js
@@ -5,12 +5,12 @@ const celebrate_1 = require("celebrate");
 exports.itemSchema = {
     [celebrate_1.Segments.BODY]: celebrate_1.Joi.object().keys({
         name: celebrate_1.Joi.string().required(),
-        description: celebrate_1.Joi.string().required(),
+        description: celebrate_1.Joi.string().default(""),
         price: celebrate_1.Joi.number().required(),
         category: celebrate_1.Joi.string().required().valid("toys", "food", "accessories"),
         serialNumber: celebrate_1.Joi.string().required(),
         avaliableQuantity: celebrate_1.Joi.number().required(),
-        allowed: celebrate_1.Joi.boolean().required(),
+        allowed: celebrate_1.Joi.boolean().default(true),
         shippingPrice: celebrate_1.Joi.number().required(),
         additionDate: celebrate_1.Joi.date()
     })
--- src/Components/Admin/middleware/validateItem.ts
+++ src/Components/Admin/middleware/validateItem.ts
@@ -3,12 +3,12 @@ import { Joi, celebrate, Segments, Modes, SchemaOptions } from "celebrate";
 export const itemSchema = {
     [Segments.BODY]: Joi.object().keys({
         name: Joi.string().required(),
-        description: Joi.string().required(),
+        description: Joi.string().default(""),
         price: Joi.number().required(),
         category: Joi.string().required().valid("toys", "food", "accessories"),
         serialNumber: Joi.string().required(),
         avaliableQuantity: Joi.number().required(),
-        allowed: Joi.boolean().required(),
+        allowed: Joi.boolean().default(true),
         shippingPrice: Joi.number().required(),
         additionDate: Joi.date()
     })
